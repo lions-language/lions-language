3acceb2691eaa5c4e3055bfb183be1a10ec0eaaa
lions-language <lions_language@163.com>
2020-10-24 16:32:32 +0800

    NOTE: delete data 实现存在问题

--- core/libvm/src/vm/memory/mod.rs
+++ core/libvm/src/vm/memory/mod.rs
@@ -4,6 +4,7 @@ use crate::vm::VirtualMachine;
 
 impl VirtualMachine {
     pub fn delete_data(&mut self, value: DeleteData) {
+        /*
         let addr = value.fields_move();
         let src_data_addr = self.thread_context.current_mut_unchecked()
             .get_data_addr_unchecked(&addr).clone();
@@ -14,6 +15,7 @@ impl VirtualMachine {
          * */
         self.thread_context.current_mut_unchecked()
             .free_data(src_data_addr);
+        */
     }
 }
 
