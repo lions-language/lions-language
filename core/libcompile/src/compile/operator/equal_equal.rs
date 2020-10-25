6a96996dcf8d812960a9dc81b83c8537a6d0c55d
MwlLj <lions_language@163.com>
2020-10-25 16:24:47 +0800

    实现 == find function

--- core/libcompile/src/compile/operator/equal_equal.rs
+++ core/libcompile/src/compile/operator/equal_equal.rs
@@ -1,28 +1,32 @@
 use libresult::*;
 use libgrammar::token::TokenValue;
-use libtype::{Type, TypeAttrubute};
-use libtype::function::{FunctionParamData, FunctionParamDataItem
-        , splice::FunctionSplice, FindFunctionContext
-        , FindFunctionResult, FunctionReturnDataAttr
-        , FunctionReturnRefParam
-        , Function, CallFunctionParamAddr
-        , CallFunctionReturnData};
-use libtype::{AddressType, AddressValue, TypeValue
-    , AddressKey};
-use libtype::package::{PackageStr};
-use libcommon::ptr::{RefPtr};
-use crate::compile::{Compile, Compiler, CallFunctionContext
-    , AddressValueExpand, OwnershipMoveContext};
-use crate::compile::value_buffer::{ValueBufferItemContext};
-use crate::compile::scope::{ScopeType};
-use crate::address::{Address};
+use libtype::function::{FunctionDefine
+        , FunctionStatement};
+use crate::compile::{Compile, Compiler};
 
 impl<'a, F: Compile> Compiler<'a, F> {
     pub fn operator_equal_equal(&mut self, _value: TokenValue) -> DescResult {
+        use libtype::function::consts;
         let (right_typ, right_addr, right_typ_attr, right_package_type, right_package_str, right_context)
             = take_value_top!(self, right_expr_value).fields_move();
         let (left_typ, left_addr, left_typ_attr, left_package_type, left_package_str, left_context)
             = take_value_top!(self, left_expr_value).fields_move();
+        let mut func_define = FunctionDefine::new_invalid_addr();
+        let mut func_statement: Option<FunctionStatement> = None;
+        let param_typs = vec![(right_typ.clone(), right_typ_attr.clone())];
+        let (exists, func_str) = 
+            match self.function_is_exist(consts::OPERATOR_EQUAL_EQUAL_FUNCTION_NAME
+            , Some(&left_typ), None
+            , param_typs, &mut func_statement, &mut func_define) {
+            Ok(r) => r,
+            Err(err) => {
+                return err;
+            }
+        };
+        if !exists {
+            return DescResult::Error(
+                format!("{} is undefine", func_str));
+        }
         DescResult::Success
     }
 }
--- core/libtype/src/function/consts.rs
+++ core/libtype/src/function/consts.rs
@@ -1,3 +1,4 @@
 pub static OPERATOR_PLUS_FUNCTION_NAME: &'static str = "+";
+pub static OPERATOR_EQUAL_EQUAL_FUNCTION_NAME: &'static str = "==";
 pub static TO_STR_FUNCTION_NAME: &'static str = "to_str";
 pub static PRIMEVAL_FUNCTION_PRINTLN: &'static str = "println";
