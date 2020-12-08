8283e996aa634b226d85cc0729156487cc3143b0
lions-language <lions_language@163.com>
2020-12-08 17:52:06 +0800

    lionsc add command

--- lionsc/src/main.rs
+++ lionsc/src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use]
+extern crate command_option;
+use command_option::flag::{Flag};
+
+const COMPILE_TYPE: &'static str = "-t";
+const COMPILE_LIB: &'static str = "--lib";
+const COMPILE_BIN: &'static str = "--bin";
+
 fn main() {
-    println!("Hello, world!");
+    let mut flag = Flag::new();
+    let compile_type = flag.reg_string(String::from(COMPILE_TYPE)
+        , String::from("localhost"), String::from("compile type --lib / --bin"));
+    flag.parse();
 }
