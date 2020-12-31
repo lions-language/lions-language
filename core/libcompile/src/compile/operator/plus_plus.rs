4bcef320e3efaf9e2897816b0cb8c8150432fef9
lions-language <lions_language@163.com>
2020-12-31 10:01:51 +0800

    add OPERATOR_PREFIX_PLUS_PLUS_FUNCTION

--- core/libcompile/src/compile/operator/plus_plus.rs
+++ core/libcompile/src/compile/operator/plus_plus.rs
@@ -11,26 +11,16 @@ use crate::compile::{Compile, Compiler};
 impl<'a, F: Compile> Compiler<'a, F> {
     pub fn prefix_plus_plus(&mut self, context: PrefixPlusPlusContext) -> DescResult {
         use libtype::function::consts;
-        let (_, desc_ctx) = context.fields_move();
-        let right = self.scope_context.top_n_with_panic_from_value_buffer(1);
-        let right_typ = right.typ_ref();
-        let right_typ_attr = right.typ_attr_ref();
-        let left = self.scope_context.top_n_with_panic_from_value_buffer(2);
-        let left_typ = left.typ_clone();
-        let left_typ_attr = left.typ_attr_ref();
-        /*
-        let (right_typ, right_addr, right_typ_attr, right_package_type, right_package_str, right_context)
-            = take_value_top!(self, right_expr_value).fields_move();
-        let (left_typ, left_addr, left_typ_attr, left_package_type, left_package_str, left_context)
-            = take_value_top!(self, left_expr_value).fields_move();
-        */
+        let (_, symbol_count) = context.fields_move();
+        let value = self.scope_context.top_n_with_panic_from_value_buffer(1);
+        let value_typ = value.typ_ref();
+        let value_typ_attr = value.typ_attr_ref();
         let mut func_define = FunctionDefine::new_invalid_addr();
         let mut func_statement: Option<FunctionStatement> = None;
-        let param_typs = vec![(left_typ.clone(), left_typ_attr.clone())
-            , (right_typ.clone(), right_typ_attr.clone())];
+        let param_typs = vec![(value_typ.clone(), value_typ_attr.clone())];
         let (exists, func_str) = 
-            match self.function_is_exist(consts::OPERATOR_EQUAL_EQUAL_FUNCTION_NAME
-            , Some(&left_typ), left.package_str_clone(), &None
+            match self.function_is_exist(consts::OPERATOR_PREFIX_PLUS_PLUS_FUNCTION_NAME
+            , Some(&value_typ), value.package_str_clone(), &None
             , param_typs, &mut func_statement, &mut func_define) {
             Ok(r) => r,
             Err(err) => {
