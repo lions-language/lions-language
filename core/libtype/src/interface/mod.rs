0ff39c174cb40666af6aabdf4c471191fcfb8186
lions-language <lions_language@163.com>
2021-02-05 18:28:32 +0800

    add interfacestatement

--- core/libtype/src/interface/mod.rs
+++ core/libtype/src/interface/mod.rs
@@ -2,12 +2,22 @@ use libmacro::{FieldGet, FieldGetClone
     , FieldGetMove, NewWithAll};
 use crate::{Type, TypeAttrubute
     , AddressType};
+use crate::function::{FunctionParan, FunctionReturn};
 use std::collections::{HashMap};
 
 #[derive(Debug, FieldGet
     , FieldGetMove, NewWithAll
     , Default)]
+pub struct InterfaceFunctionStatement {
+    func_param: Option<Vec<FunctionParam>>,
+    func_return: Option<FunctionReturn>
+}
+
+#[derive(Debug, FieldGet
+    , FieldGetMove, NewWithAll
+    , Default)]
 pub struct InterfaceDefine {
-    name: String
+    name: String,
+    function_define: Option<Vec<InterfaceFunctionStatement>>
 }
 
