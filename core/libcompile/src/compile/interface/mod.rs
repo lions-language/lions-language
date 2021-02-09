0bc006726e1ec01fbf42b245b5d608210e582c8f
lions-language <lions_language@163.com>
2021-02-09 18:16:45 +0800

    add param data to interface define

--- core/libcompile/src/compile/interface/mod.rs
+++ core/libcompile/src/compile/interface/mod.rs
@@ -8,7 +8,8 @@ use libgrammar::grammar::{FunctionDefineParamContext
     , InterfaceFunctionStatementContext
     , TypeToken};
 use libtype::function::{AddFunctionContext
-    , FunctionParamDataItem
+    , FunctionParamDataItem, FunctionParam
+    , FunctionParamData
     , FunctionReturnData, FunctionReturn
     , FunctionStatement};
 use libtype::{TypeAttrubute, Type
@@ -38,10 +39,61 @@ impl<'a, F: Compile> Compiler<'a, F> {
     pub fn process_interface_function_define_param(&mut self
         , define: &mut InterfaceDefine, context: FunctionDefineParamContext)
         -> DescResult {
-        match define.func_param_mut() {
-            Some(params) => {
+        let func_statement = match define.function_statement_mut() {
+            Some(statements) => {
+                if statements.is_empty() {
+                    panic!("function statement is empty");
+                }
+                statements.last_mut().unwrap()
             },
             None => {
+                panic!("function statement is none");
+            }
+        };
+        let (name_token, t, typ_attr, lengthen_attr, param_no)
+            = context.fields_move();
+        let typ = match t {
+            FunctionDefineParamContextType::Token(token) => {
+                match self.to_type(token) {
+                    Ok(ty) => ty,
+                    Err(err) => {
+                        return err;
+                    }
+                }
+            },
+            FunctionDefineParamContextType::Typ(ty) => {
+                ty
+            }
+        };
+        let item = FunctionParamDataItem{
+            typ: typ,
+            typ_attr: typ_attr,
+            lengthen_attr: lengthen_attr,
+            is_auto_call_totype: false,
+            is_check_func_call_param_typ_attr: true
+        };
+        match func_statement.func_param_mut() {
+            Some(param) => {
+                match &mut param.data {
+                    FunctionParamData::Single(p) => {
+                        /*
+                         * 将 single 转换为 multi
+                         * */
+                        let params = vec![p.clone(), item];
+                        *&mut param.data = FunctionParamData::Multi(params);
+                    },
+                    FunctionParamData::Multi(ps) => {
+                        ps.push(item);
+                    }
+                }
+            },
+            None => {
+                /*
+                 * 没有参数 => 设置为一个参数
+                 * */
+                *func_statement.func_param_mut() = Some(FunctionParam{
+                    data: FunctionParamData::Single(item)
+                });
             }
         }
         DescResult::Success
