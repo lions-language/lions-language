afeb614ea4c7dfd9c58badf52b99e6b3ff95dbdc
lions-language <lions_language@163.com>
2021-02-25 19:48:50 +0800

    实现 compile find interface end

--- core/libcompile/src/compile/finder/mod.rs
+++ core/libcompile/src/compile/finder/mod.rs
@@ -22,6 +22,27 @@ impl<'a, F: Compile> Compiler<'a, F> {
 
     pub fn process_find_interface_end(&mut self, end_context: FindInterfaceEndContext
                                       , context: &mut FindInterfaceContext) -> DescResult {
+        if context.context_ref().is_null() {
+            /*
+             * 无前缀
+             * */
+            let module_str = self.module_stack.current().name_ref();
+        } else {
+            /*
+             * 存在前缀
+             * */
+            let module_prefix = context.context_mut().pop::<String>();
+            let (module_str, package_str) = match self.imports_mapping.get_clone(&module_prefix) {
+                Some(v) => {
+                    let (ms, ps) = v.fields_move();
+                    (Some(ms), ps)
+                },
+                None => {
+                    return DescResult::Error(
+                        format!("{} is not found", module_prefix));
+                }
+            };
+        }
         DescResult::Success
     }
 }
