9b3e4d22b134962c5ea510b7a79fe76e0b9e1453
lions-language <lions_language@163.com>
2021-03-23 20:47:09 +0800

    将 is 对应的操作符重载函数名改为 opt_is, 否则会和 is 关键字冲突

--- core/libtype/src/function/consts.rs
+++ core/libtype/src/function/consts.rs
@@ -3,6 +3,6 @@ pub static OPERATOR_EQUAL_EQUAL_FUNCTION_NAME: &'static str = "==";
 pub static OPERATOR_PREFIX_PLUS_PLUS_FUNCTION_NAME: &'static str = "++x";
 pub static OPERATOR_LESS_THAN_FUNCTION_NAME: &'static str = "<";
 pub static OPERATOR_TWO_POINT_FUNCTION_NAME: &'static str = "..";
-pub static OPERATOR_IS_FUNCTION_NAME: &'static str = "is";
+pub static OPERATOR_IS_FUNCTION_NAME: &'static str = "opt_is";
 pub static TO_STR_FUNCTION_NAME: &'static str = "to_str";
 pub static PRIMEVAL_FUNCTION_PRINTLN: &'static str = "println";
