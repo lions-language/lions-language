b0708b477d9bea05ea904024686213482043d724
lions-language <lions_language@163.com>
2021-04-07 21:19:06 +0800

    拆分 process_boolean

--- core/libcompile/src/compile/boolean/mod.rs
+++ core/libcompile/src/compile/boolean/mod.rs
@@ -1,14 +1,18 @@
 // use libgrammar::token::{TokenData};
 use libgrammar::grammar::{ConstBooleanContext};
 use libtype::{Type, TypeValue, Primeval
-    , AddressType
-    , primeval::PrimevalType};
+    , AddressType, TypeAttrubute
+    , primeval::PrimevalType, Data};
 use crate::compile::{Compile, Compiler, TokenValueExpand
     , LoadStackContext};
 
 impl<'a, F: Compile> Compiler<'a, F> {
     pub fn process_const_boolean(&mut self, context: ConstBooleanContext) {
         let (value, typ_attr) = context.fields_move();
+        self.const_boolean(value.to_data(), typ_attr);
+    }
+
+    fn const_boolean(&mut self, data: Data, typ_attr: TypeAttrubute) {
         let typ = Type::new(TypeValue::Primeval(Primeval::new(
                     PrimevalType::Boolean)), typ_attr.clone());
         /*
@@ -25,7 +29,7 @@ impl<'a, F: Compile> Compiler<'a, F> {
          * 生成读取静态量的指令, 虚拟机接收到这个指令后, 将地址写入到计算栈中
          * */
         let context = LoadStackContext::new_with_all(
-            addr.addr(), value.to_data());
+            addr.addr(), data);
         self.cb.load_stack(context);
     }
 }
