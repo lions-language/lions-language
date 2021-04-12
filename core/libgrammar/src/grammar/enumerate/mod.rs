23e6c95208fbea2cd55cfe8b5d0ba0b74ca2d5b4
lions-language <lions_language@163.com>
2021-04-12 22:18:55 +0800

    parse enum name

--- core/libgrammar/src/grammar/enumerate/mod.rs
+++ core/libgrammar/src/grammar/enumerate/mod.rs
@@ -11,6 +11,27 @@ impl<'a, T: FnMut() -> CallbackReturnStatus, CB: Grammar> GrammarParser<'a, T, C
          * 跳过 enum 关键字
          * */
         self.skip_next_one();
+        let tp = match self.skip_white_space_token() {
+            Some(tp) => {
+                tp
+            },
+            None => {
+                self.panic("expect id after interface, but arrive EOF");
+                panic!();
+            }
+        };
+        let next = tp.as_ref::<T, CB>();
+        if let TokenType::Id = next.context_token_type() {
+        } else {
+            self.panic(&format!("expect id after interface, but meet {:?}"
+                , next.context_token_type()));
+        }
+        let next_token = self.take_next_one();
+        let name = extract_token_data!(next_token.token_value().token_data_unchecked(), Id);
+        self.enum_block_process();
+    }
+
+    fn enum_block_process(&mut self) {
     }
 }
 
