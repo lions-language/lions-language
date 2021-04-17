911adc279220c0ffbc9fddf90cb4ff1946bd4f5a
lions-language <lions_language@163.com>
2021-04-17 10:39:49 +0800

    update id_access

--- core/libgrammar/src/grammar/id_access/mod.rs
+++ core/libgrammar/src/grammar/id_access/mod.rs
@@ -8,5 +8,16 @@ impl<'a, T: FnMut() -> CallbackReturnStatus, CB: Grammar> GrammarParser<'a, T, C
          * env:
          *  1. 从 math 开始, 不要跳过 math
          * */
+        let tp = self.skip_white_space_token();
+        let token = tp.as_ref::<T, CB>();
+        match token.context_token_type() {
+            TokenType::Id => {
+                return self.enum_block_item();
+            },
+            _ => {
+                self.panic(&format!("expect id, but meet {:?}", token.context_token_type()));
+                panic!();
+            }
+        }
     }
 }
