1ca3ca3a23b7b452b8e32c6cb72d19b74fcb411c
lions-language <lions_language@163.com>
2021-04-18 15:44:31 +0800

    update id_access

--- core/libgrammar/src/grammar/id_access/mod.rs
+++ core/libgrammar/src/grammar/id_access/mod.rs
@@ -1,5 +1,6 @@
 use crate::lexical::{CallbackReturnStatus};
 use crate::grammar::{GrammarParser, Grammar};
+use crate::token::{TokenType, TokenValue, TokenData};
 
 impl<'a, T: FnMut() -> CallbackReturnStatus, CB: Grammar> GrammarParser<'a, T, CB> {
     pub fn format_define(&mut self) {
@@ -12,12 +13,36 @@ impl<'a, T: FnMut() -> CallbackReturnStatus, CB: Grammar> GrammarParser<'a, T, C
         let token = tp.as_ref::<T, CB>();
         match token.context_token_type() {
             TokenType::Id => {
-                return self.enum_block_item();
+                self.format_define_id();
             },
             _ => {
                 self.panic(&format!("expect id, but meet {:?}", token.context_token_type()));
                 panic!();
             }
         }
+    }
+
+    fn format_define_id(&mut self) {
+        let first = self.take_next_one().token_value().token_data_unchecked();
+        let first_name = extract_token_data!(first, Id);
+        let tp = self.skip_white_space_token();
+        let token = tp.as_ref::<T, CB>();
+        match token.context_token_type() {
+            TokenType::ColonColon => {
+                self.format_define_colon_colon(first_name);
+            },
+            TokenType::RightParenthese => {
+                self.format_define_single(first_name);
+            },
+            _ => {
+                self.panic(&format!("expect id or right_parenthese, but meet {:?}", token.context_token_type()));
+            }
+        }
+    }
+
+    fn format_define_colon_colon(&mut self, name: String) {
+    }
+
+    fn format_define_single(&mut self, name: String) {
     }
 }
