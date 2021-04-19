ed9a8548e0ad87f4694180475220a940e3627bcf
lions-language <lions_language@163.com>
2021-04-19 22:24:52 +0800

    update id_access

--- core/libgrammar/src/grammar/id_access/mod.rs
+++ core/libgrammar/src/grammar/id_access/mod.rs
@@ -3,7 +3,7 @@ use crate::grammar::{GrammarParser, Grammar};
 use crate::token::{TokenType, TokenValue, TokenData};
 
 impl<'a, T: FnMut() -> CallbackReturnStatus, CB: Grammar> GrammarParser<'a, T, CB> {
-    pub fn format_define(&mut self) {
+    pub fn format_define(&mut self, end_token: TokenType) {
         /*
          * 处理如: a is math::Get
          * env:
@@ -13,7 +13,7 @@ impl<'a, T: FnMut() -> CallbackReturnStatus, CB: Grammar> GrammarParser<'a, T, C
         let token = tp.as_ref::<T, CB>();
         match token.context_token_type() {
             TokenType::Id => {
-                self.format_define_id();
+                self.format_define_id(end_token);
             },
             _ => {
                 self.panic(&format!("expect id, but meet {:?}", token.context_token_type()));
@@ -22,7 +22,7 @@ impl<'a, T: FnMut() -> CallbackReturnStatus, CB: Grammar> GrammarParser<'a, T, C
         }
     }
 
-    fn format_define_id(&mut self) {
+    fn format_define_id(&mut self, end_token: TokenType) {
         let first = self.take_next_one().token_value().token_data_unchecked();
         let first_name = extract_token_data!(first, Id);
         let tp = self.skip_white_space_token();
@@ -31,18 +31,38 @@ impl<'a, T: FnMut() -> CallbackReturnStatus, CB: Grammar> GrammarParser<'a, T, C
             TokenType::ColonColon => {
                 self.format_define_colon_colon(first_name);
             },
-            TokenType::RightParenthese => {
-                self.format_define_single(first_name);
-            },
             _ => {
-                self.panic(&format!("expect id or right_parenthese, but meet {:?}", token.context_token_type()));
+                if end_token == token.context_token_type() {
+                    self.format_define_end_single(first_name);
+                } else {
+                    self.panic(&format!("expect id or {:?}, but meet {:?}", end_token, token.context_token_type()));
+                }
             }
         }
     }
 
     fn format_define_colon_colon(&mut self, name: String) {
+        /*
+         * 跳过 ::
+         * */
+        self.skip_next_one();
+        let tp = self.skip_white_space_token();
+        let token = tp.as_ref::<T, CB>();
+        match token.context_token_type() {
+            TokenType::Id => {
+                self.format_define_end_multi(name);
+            },
+            _ => {
+                self.panic(&format!("expect id, but meet {:?}", token.context_token_type()));
+            }
+        }
     }
 
-    fn format_define_single(&mut self, name: String) {
+    fn format_define_end_single(&mut self, name: String) {
+    }
+
+    fn format_define_end_multi(&mut self, first: String) {
+        let second = self.take_next_one().token_value().token_data_unchecked();
+        let second_name = extract_token_data!(second, Id);
     }
 }
