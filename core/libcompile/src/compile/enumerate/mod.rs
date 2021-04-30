e1a02110cf4ad2236b40579b8e90a92e2f8a560f
lions-language <lions_language@163.com>
2021-04-30 20:50:09 +0800

    update enumerate

--- core/libcompile/src/compile/enumerate/mod.rs
+++ core/libcompile/src/compile/enumerate/mod.rs
@@ -7,11 +7,20 @@ use crate::compile::{Compile, Compiler};
 impl<'a, F: Compile> Compiler<'a, F> {
     pub fn process_enum_define_start(&mut self
         , context: EnumDefineStartContext, define: &mut EnumerateDefine) -> DescResult {
+        *define.name_mut() = context.name();
         DescResult::Success
     }
 
     pub fn process_enum_define_item(&mut self
         , context: EnumDefineItemContext, define: &mut EnumerateDefine) -> DescResult {
+        let (name, format_define) = context.fields_move();
+        match define.items_mut() {
+            Some(item) => {
+                // item.push_back()
+            },
+            None => {
+            }
+        }
         DescResult::Success
     }
 
