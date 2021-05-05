19845070b7e037a090961923581e4dbdf7b3c854
lions-language <lions_language@163.com>
2021-05-05 08:27:53 +0800

    add anno

--- core/libcompile/src/compile/format_define/mod.rs
+++ core/libcompile/src/compile/format_define/mod.rs
@@ -5,6 +5,9 @@ use crate::compile::{Compile, Compiler};
 impl<'a, F: Compile> Compiler<'a, F> {
     pub fn process_format_define(&mut self, format_define: FormatDefine) {
         let (prefix, name) = format_define.fields_move();
+        /*
+         * 可能是 interface, 也可能是 struct, 还可能是 enum
+         * */
         match prefix {
             Some(p) => {
             },
